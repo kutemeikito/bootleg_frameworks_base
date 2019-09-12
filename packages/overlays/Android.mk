e7791ed5bc1bc44311856946bed2cd5121855d27
maxwen <max.weninger@gmail.com>
2019-09-12 23:43:37 +0200

    overlay: add IconShapeCircleOverlay
    and remove all other changes aside from icon mask
    
    Change-Id: Idb76d9c8c0844242b8b80f4838b400bb93bebba8

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -46,6 +46,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedSystemUIOverlay \
 	IconPackRoundedThemePickerOverlay \
 	IconShapePebbleOverlay \
+	IconShapeCircleOverlay \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquircleOverlay \
 	IconShapeTaperedRectOverlay \
