eyosen/frameworks_base

SystemUI: Allow customizing footer text [1/2]
Change-Id: I08b080914dc8041776eaf88333f338d505ed9f88

packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -180,8 +180,12 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
         boolean showFooterText = Settings.System.getIntForUser(mContext.getContentResolver(),
                         Settings.System.AICP_FOOTER_TEXT_SHOW, 0,
                         UserHandle.USER_CURRENT) == 1;
+        String footerText = Settings.System.getStringForUser(mContext.getContentResolver(),
+                        Settings.System.AICP_FOOTER_TEXT_STRING, UserHandle.USER_CURRENT);
+
         if (showFooterText) {
-            v.setText(mContext.getResources().getString(R.string.qs_footer_aicp_text));
+            v.setText((footerText != null && !footerText.isEmpty()) ? footerText :
+                            mContext.getResources().getString(R.string.qs_footer_aicp_text));
             v.setVisibility(View.VISIBLE);
         } else {
             v.setVisibility(View.GONE);
@@ -269,6 +273,9 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
         super.onAttachedToWindow();
         mContext.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.AICP_FOOTER_TEXT_SHOW), false,
+                mAicpSettingsObserver, UserHandle.USER_ALL);
+        mContext.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.AICP_FOOTER_TEXT_STRING), false,
                 mAicpSettingsObserver, UserHandle.USER_ALL);
 
         final TunerService tunerService = Dependency.get(TunerService.class);

kutemeikito/bootleg_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -177,8 +177,12 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
         boolean showFooterText = Settings.System.getIntForUser(mContext.getContentResolver(),
                         Settings.System.FOOTER_TEXT_SHOW, 0,
                         UserHandle.USER_CURRENT) == 1;
+        String footerText = Settings.System.getStringForUser(mContext.getContentResolver(),
+                        Settings.System.FOOTER_TEXT_STRING, UserHandle.USER_CURRENT);
+
         if (showFooterText) {
-            v.setText("#" + buildType);
+            String btFooterText = "#" + buildType;
+            v.setText((footerText != null && !footerText.isEmpty()) ? footerText : btFooterText);
             v.setVisibility(View.VISIBLE);
         } else {
             v.setVisibility(View.GONE);
@@ -264,6 +268,9 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
         super.onAttachedToWindow();
         mContext.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.FOOTER_TEXT_SHOW), false,
+                mFootextSettingsObserver, UserHandle.USER_ALL);
+        mContext.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.FOOTER_TEXT_STRING), false,
                 mFootextSettingsObserver, UserHandle.USER_ALL);
     }
 