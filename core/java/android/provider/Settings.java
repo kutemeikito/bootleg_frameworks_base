2610be1ac3e06ec46a1cac6e147ad0ee3b301012
HrX03 <dn.bianco03@gmail.com>
2020-06-11 20:40:49 +0200

    fwb: Implement VolumePluginManager
    Change-Id: I29bfc091df7a13ece3c859310e10c75daffc064d
    Signed-off-by: Ziauddin Sameer <ziauddinsameer@gmail.com>
    Signed-off-by: Akash <akashniki@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4981,6 +4981,12 @@ public final class Settings {
         public static final String ADAPTIVE_PLAYBACK_TIMEOUT = "adaptive_playback_timeout";
 
         /**
+         * Holds current enabled volume panel implementation
+         * @hide
+         */
+        public static final String SYSTEMUI_PLUGIN_VOLUME = "systemui_plugin_volume";
+
+        /**
          * These are all public system settings
          *
          * @hide
@@ -5096,6 +5102,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(FOD_GESTURE);
             PRIVATE_SETTINGS.add(FOOTER_TEXT_SHOW);
             PRIVATE_SETTINGS.add(FOOTER_TEXT_STRING);
+            PRIVATE_SETTINGS.add(SYSTEMUI_PLUGIN_VOLUME);
         }
 
         /**
