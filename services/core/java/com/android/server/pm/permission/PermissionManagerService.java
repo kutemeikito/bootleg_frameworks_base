258561c45070368bec7e02f34a35cb9e31db09ff
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-26 18:31:36 -0300

    PermissionManagerService: Correct don't enforce privapp perms for updated system apps
    Issue:
    
    java.lang.IllegalStateException: Signature|privileged permissions not in privapp-permissions whitelist: {com.google.android.projection.gearhead (/data/app/~~WSwlGmikwcVQrwQDXwWdAA==/com.google.android.projection.gearhead-bzzvY3HIOB9kXhNRPB2AfA==)}
    
    Somehow pkgSetting.getPkgState().isUpdatedSystemApp() is returning false, so device enters into bootloop state due to that bad behavior.
    
    Change-Id: I1e9fb1175473e8c8ec1e5e3c519947d3414c57ee

--- services/core/java/com/android/server/pm/permission/PermissionManagerService.java
+++ services/core/java/com/android/server/pm/permission/PermissionManagerService.java
@@ -3556,7 +3556,8 @@ public class PermissionManagerService extends IPermissionManager.Stub {
                 // Only enforce whitelist this on boot
                 if (!mSystemReady
                         // Updated system apps do not need to be whitelisted
-                        && !pkgSetting.getPkgState().isUpdatedSystemApp()) {
+                        && !pkgSetting.getPkgState().isUpdatedSystemApp()
+                        && !pkg.getCodePath().startsWith("/data/app/")) {
                     ApexManager apexMgr = ApexManager.getInstance();
                     String apexContainingPkg = apexMgr.getActiveApexPackageNameContainingPackage(
                             pkg);
@@ -3580,6 +3581,7 @@ public class PermissionManagerService extends IPermissionManager.Stub {
                             deniedPermissions = SystemConfig.getInstance()
                                     .getPrivAppDenyPermissions(pkg.getPackageName());
                         }
+
                         final boolean permissionViolation =
                                 deniedPermissions == null || !deniedPermissions.contains(perm);
                         if (permissionViolation) {
