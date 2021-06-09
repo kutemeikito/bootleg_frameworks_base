yaap/frameworks_base

base: Allow hiding notification headers [1/2]
Gimme back my space, thank you

Change-Id: I5f63a384d2bd2052d4a8a9d49aff1456d743367f

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -101,5 +101,6 @@ public class SystemSettings {
         Settings.System.QS_FOOTER_TEXT_STRING,
         Settings.System.QS_SHOW_BRIGHTNESS,
         Settings.System.QS_BRIGHTNESS_POSITION_BOTTOM,
+        Settings.System.NOTIFICATION_HEADERS,
     };
 }

kutemeikito/bootleg_frameworks_base

Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -88,6 +88,7 @@ public class SystemSettings {
         Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN,
         Settings.System.DOZE_ON_CHARGE,
         Settings.System.RINGTONE_VIBRATION_PATTERN,
-        Settings.System.AUTHENTICATION_SUCCESS_VIB
+        Settings.System.AUTHENTICATION_SUCCESS_VIB,
+        Settings.System.NOTIFICATION_HEADERS
     };
 }